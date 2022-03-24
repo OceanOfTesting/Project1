78b13d5ac5e5e5837807a324483e5247464a49d5
Techieautomation <twinklepatnaik116@gmail.com>
2022-03-24 18:01:57 +0530

    first commit

---
+++ src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,6 @@
+package com.pages;
+
+public class LoginPage {
+	public void loginPage() {
+		System.out.println("This is login page");
+}
---
+++ src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,6 @@
+package com.testcases;
+
+public class LoginTest {
+	public void loginTest() {
+		System.out.println("This is login test");
+}
